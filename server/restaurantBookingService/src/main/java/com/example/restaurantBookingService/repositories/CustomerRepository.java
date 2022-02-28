789dd1203abbf990453ba9cf1faf2cedae1d0636
Amna <amna.bashir2@gmail.com>
2022-02-28 15:27:41 +0000

    create models for customers and loyality card

--- server/restaurantBookingService/src/main/java/com/example/restaurantBookingService/repositories/CustomerRepository.java
+++ server/restaurantBookingService/src/main/java/com/example/restaurantBookingService/repositories/CustomerRepository.java
@@ -1,4 +1,9 @@
 package com.example.restaurantBookingService.repositories;
 
-public interface CustomerRepository {
+import com.example.restaurantBookingService.models.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
