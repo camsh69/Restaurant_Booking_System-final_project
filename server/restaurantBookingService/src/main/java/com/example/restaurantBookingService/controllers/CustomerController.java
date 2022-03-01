6227791c911820a405f5c9288be2159b042ed58c
Amna <amna.bashir2@gmail.com>
2022-03-01 09:41:58 +0000

    controllers_repositories and annotation done for customers and loyaltycard

--- server/restaurantBookingService/src/main/java/com/example/restaurantBookingService/controllers/CustomerController.java
+++ server/restaurantBookingService/src/main/java/com/example/restaurantBookingService/controllers/CustomerController.java
@@ -1,4 +1,49 @@
 package com.example.restaurantBookingService.controllers;
 
+import com.example.restaurantBookingService.models.Customer;
+import com.example.restaurantBookingService.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.concurrent.Callable;
+
+@RestController
 public class CustomerController {
-}
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @GetMapping(value = "/customers")
+    public ResponseEntity<List<Customer>>getAllCustomers(){
+        return new ResponseEntity<>(customerRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/customers/{id}")
+    public ResponseEntity getCustomer(@PathVariable Long id){
+        return new ResponseEntity<>(customerRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/customers")
+    public ResponseEntity<Customer> postCustomer(@RequestBody Customer customer){
+        customerRepository.save(customer);
+        return new ResponseEntity<>(customer, HttpStatus.CREATED);
+    }
+
+    @PatchMapping(value = "/customers/{id}")
+    public ResponseEntity<Customer> updateCustomer(@RequestBody Customer customer){
+        customerRepository.save(customer);
+        return new ResponseEntity<>(customer, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/customers/{id}")
+    public ResponseEntity<Customer> deleteCustomer(@PathVariable Long id){
+        Customer found = customerRepository.getOne(id);
+        customerRepository.delete(found);
+        return new ResponseEntity<>(null, HttpStatus.OK);
+        };
+    }
+
+
--- server/restaurantBookingService/src/test/java/com/example/restaurantBookingService/RestaurantBookingServiceApplicationTests.java
+++ server/restaurantBookingService/src/test/java/com/example/restaurantBookingService/RestaurantBookingServiceApplicationTests.java
@@ -1,10 +1,23 @@
 package com.example.restaurantBookingService;
 
+import com.example.restaurantBookingService.models.Customer;
+import com.example.restaurantBookingService.models.LoyaltyCard;
+import com.example.restaurantBookingService.repositories.CustomerRepository;
+import com.example.restaurantBookingService.repositories.LoyaltyCardRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.Callable;
 
 @SpringBootTest
 class RestaurantBookingServiceApplicationTests {
+
+	@Autowired
+	CustomerRepository customerRepository;
+
+	@Autowired
+	LoyaltyCardRepository loyaltyCardRepository;
 
 	@Test
 	void contextLoads() {
