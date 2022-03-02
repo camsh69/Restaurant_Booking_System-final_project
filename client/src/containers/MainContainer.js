8e8aebdaacf5ffb334bd1739eee5223df87a2622
Toms Druvaskalns <tomsdruvas@gmail.com>
2022-03-02 14:08:51 +0000

    made react app

---
+++ client/src/containers/MainContainer.js
@@ -0,0 +1,35 @@
+import React, {useState, useEffect} from 'react';
+import Request from '../helpers/request';
+
+
+const MainContainer = () => {
+
+    const [customers, setCustomers] = useState([]);
+    const [bookings, setBookings] = useState([]);
+    const [restaurantTables, setRestaurantTables] = useState([]);
+  
+    const requestAll = function(){
+      const request = new Request();
+      const customerPromise = request.get('/api/customers')
+      const bookingPromise = request.get('/api/bookings')
+      const restaurantTablePromise = request.get('/api/restaurantTables')
+  
+      Promise.all([customerPromise, bookingPromise, restaurantTablePromise])
+      .then((data) => {
+        setCustomers(data[0]);
+        setBookings(data[1]);
+        setRestaurantTables(data[2])
+      })
+    }
+  
+    useEffect(()=>{
+      requestAll()
+    }, [])
+
+
+  return (
+    <div>MainContainer</div>
+  )
+}
+
+export default MainContainer
