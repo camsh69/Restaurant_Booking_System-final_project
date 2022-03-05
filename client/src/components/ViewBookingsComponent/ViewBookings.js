d92314bba9c011a62e7a2e49d3d18c8c321285cd
Campbell Forsyth <camsh69@gmail.com>
2022-03-05 19:04:43 +0000

    code tidy up and added add booking alert box

--- client/src/components/ViewBookingsComponent/ViewBookings.js
+++ client/src/components/ViewBookingsComponent/ViewBookings.js
@@ -7,41 +7,60 @@ import withReactContent from 'sweetalert2-react-content';
 
 const localizer = momentLocalizer(moment);
 
-const MySwal = withReactContent(Swal);
+const fireSwal = withReactContent(Swal);
 
-const ViewBookings = ({events, resourceMap, minTime, maxTime}) => {
+const minTime = new Date();
+minTime.setHours(13, 0, 0);
 
-    return (
-        <div className="App">
-          <Calendar
-            localizer={localizer}
-            events={events}
-            defaultView={Views.DAY}
-            views={['day', 'work_week']}
-            defaultDate={moment().toDate()}
-            startAccessor="start"
-            endAccessor="end"
-            resources={resourceMap}
-            resourceIdAccessor="resourceId"
-            resourceTitleAccessor="resourceTitle"
-            onSelectEvent={event => MySwal.fire({
-            icon: 'info',
-            title: `Booking Ref: ${event.ref}`,
-            text: `Contact: ${event.contact}, 
-            Phone: ${event.phone}, 
-            Email: ${event.email},   
-            Party size: ${event.diners},   
-            Special message: ${event.message}   LoyaltyCard: ${event.loyaltyCard}`,
-            footer: '<strong><a href="/edit">Edit or Delete this Booking</a></strong>',
-            confirmButtonColor: '#2A67BC',
-            confirmButtonText: "Close",
-            iconColor: '#A3C4D9'})}
-            min={minTime}
-            max={maxTime}
-            style={{ height: "75vh" }}
-          />
-        </div>
-      );
+const maxTime = new Date();
+maxTime.setHours(23, 59, 59);
+
+const ViewBookings = ({events, resourceMap}) => {  
+
+  return (
+      <div className="App">
+        <Calendar
+          selectable={true}
+          localizer={localizer}
+          events={events}
+          defaultView={Views.DAY}
+          views={['day', 'month']}
+          defaultDate={moment().toDate()}
+          startAccessor="start"
+          endAccessor="end"
+          resources={resourceMap}
+          resourceIdAccessor="resourceId"
+          resourceTitleAccessor="resourceTitle"
+          onSelectSlot={(slotInfo) => fireSwal.fire({
+          icon: 'question',
+          title: 'Add A New Booking?',
+          html: '<strong><a href="/add">Click to Add New Booking</a></strong>',
+          confirmButtonColor: '#2A67BC',
+          confirmButtonText: "Cancel",
+          iconColor: '#A3C4D9',
+          showCloseButton: 'true'
+          })}
+          onSelectEvent={event => fireSwal.fire({
+          icon: 'info',
+          title: `Booking Ref: ${event.ref}`,
+          text: `Contact: ${event.contact}, 
+          Phone: ${event.phone}, 
+          Email: ${event.email},   
+          Party size: ${event.diners},   
+          Special message: ${event.message}   LoyaltyCard: ${event.loyaltyCard}`,
+          footer: '<strong><a href="/edit">Click to Edit or Delete this Booking</a></strong>',
+          confirmButtonColor: '#2A67BC',
+          confirmButtonText: "Close",
+          iconColor: '#A3C4D9',
+          showCloseButton: 'true'
+
+          })}
+          min={minTime}
+          max={maxTime}
+          style={{ height: "75vh" }}
+        />
+      </div>
+    );
 }
 
-export default ViewBookings
+export default ViewBookings;
