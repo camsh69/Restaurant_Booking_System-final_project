2878c5897e61e1c4c0b10e926b6790b64f09fe02
saimeerasingh <singhsaimeera@gmail.com>
2022-03-06 22:02:29 +0000

    loyalty card fetch setup

--- client/src/components/BookingFormComponent/CustomerForm.js
+++ client/src/components/BookingFormComponent/CustomerForm.js
@@ -1,105 +1,108 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 
 
-  const CustomerForm = ({newCustomer}) => {
-  const [name, setName] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [email, setEmail] = useState("");
-  const [customerId, setCustomerId] = useState("");
+const CustomerForm = ({ newCustomer }) => {
+    const [name, setName] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [email, setEmail] = useState("");
+    const [customerId, setCustomerId] = useState("");
+    const [loyaltyCard, setLoyaltyCard] = useState("")
 
-  const handleNameChange = (ev) => setName(ev.target.value);
-  const handlePhoneNumberChange = (ev) => setPhoneNumber(ev.target.value);
-  const handleEmailChange = (ev) => setEmail(ev.target.value);
+    const handleNameChange = (ev) => setName(ev.target.value);
+    const handlePhoneNumberChange = (ev) => setPhoneNumber(ev.target.value);
+    const handleEmailChange = (ev) => setEmail(ev.target.value);
+    const handleLoyaltyCard = (ev) => setLoyaltyCard(ev.target.value);
+
+
+    function sayHello(loyaltyCard) {
+        fetch("http://localhost:8080/api/customers?loyaltyCard=" + loyaltyCard , {
+            "method": "GET",
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+            })
+            .catch(err => {
+                console.error(err);
+            });
+      }
 
 
 
+    const handleSubmit = ev => {
+        ev.preventDefault();
+        newCustomer({
+            name: name,
+            phoneNumber: phoneNumber,
+            email: email
+        });
+        setName("");
+        setPhoneNumber("");
+        setEmail("");
 
-  const getCustomerFromLoyalty = function(loyalty_card){
-      var loyalty_card = "036758";
-      fetch("http://localhost:8080/api/customers?loyaltyCard=036758&", {
-        "method": "GET",
- 
-      })
-      .then(response => {
-        console.log(response);
-      })
-      .catch(err => {
-        console.error(err);
-      });
+    }
+    return (
+        <div>
+            <div>
+                {/* text box of laoyal with onChange event to hamdleLoyalty */}
+                <input
+                    type="text"
+                    id="loyalty_card"
+                    name="loyalty_card"
+                    value={loyaltyCard}
+                    onChange={handleLoyaltyCard}
+                />
+            </div>
+            <div>
+                <button onClick={() => sayHello(loyaltyCard)}>Greet</button>
+            </div>
+
+            <form onSubmit={handleSubmit}>
+                <h1>Add a Customer</h1>
+                <div className="group">
+                    <label htmlFor="name">Guest Name:</label>
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        value={name}
+                        required
+                        onChange={handleNameChange}
+                    />
+                </div>
+
+                <div className="group">
+                    <label htmlFor="phoneNumber">Guest Phone number:</label>
+                    <input
+                        type="phoneNumber"
+                        id="phoneNumber"
+                        name="phoneNumber"
+                        value={phoneNumber}
+                        required
+                        onChange={handlePhoneNumberChange}
+                    />
+                </div>
+
+                <div className="group">
+                    <label htmlFor="email">Guest Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        value={email}
+                        required
+                        onChange={handleEmailChange}
+                    />
+                </div>
+
+                <input type="submit" name="submit" value="Save" />
+            </form>
+        </div>
+
+    );
+
 }
-
-  const handleLoyalty = ev => {
-    getCustomerFromLoyalty("")
-  }
-
-  const handleSubmit = ev => {
-    ev.preventDefault();
-   newCustomer({
-      name: name,
-      phoneNumber : phoneNumber,
-      email: email
-          });
-    setName("");
-    setPhoneNumber("");
-    setEmail("");
-    
-  }
-  return (
-      <div>
-        {/* text box of laoyal with onChange event to hamdleLoyalty */}
-        <input 
-          type="text" 
-          id="loyalty_card" 
-          name="loyalty_card" 
-          value=""
-          onChange={handleLoyalty}
-        />
-    <form onSubmit={handleSubmit}>
-      <h1>Add a Customer</h1>
-      <div className="group">
-        <label htmlFor="name">Guest Name:</label>
-        <input 
-          type="text" 
-          id="name" 
-          name="name" 
-          value={name} 
-          required 
-          onChange={handleNameChange}
-        />
-      </div>
-     
-      <div className="group">
-        <label htmlFor="phoneNumber">Guest Phone number:</label>
-        <input 
-          type="phoneNumber" 
-          id="phoneNumber" 
-          name="phoneNumber" 
-          value={phoneNumber} 
-          required 
-          onChange={handlePhoneNumberChange}
-        />
-      </div>
-
-      <div className="group">
-        <label htmlFor="email">Guest Email:</label>
-        <input 
-          type="email" 
-          id="email" 
-          name="email" 
-          value={email} 
-          required 
-          onChange={handleEmailChange}
-        />
-      </div>
-      
-      <input type="submit" name="submit" value="Save" />
-    </form>
-    </div>
-
-  );
-
-  }
 
 
 export default CustomerForm;
