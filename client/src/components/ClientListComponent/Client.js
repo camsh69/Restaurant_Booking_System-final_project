5cb859d75adb16806c6b7588e1fb08b610003bba
Toms Druvaskalns <tomsdruvas@gmail.com>
2022-03-08 13:54:57 +0000

    branch aim finished

--- client/src/components/ClientListComponent/Client.js
+++ client/src/components/ClientListComponent/Client.js
@@ -1,14 +1,11 @@
 import React, {useState} from 'react';
 
 const Client = ({customer}) => {
-    
-
-
 
   return (
     <div>
     <a href="#">{customer.name}</a>&nbsp;&nbsp;
-    Number of bookings:&nbsp;{customer.bookings.length} <button>View Bookings</button>
+    Number of bookings:&nbsp;{customer.bookings.length}
     </div>
 
     )
